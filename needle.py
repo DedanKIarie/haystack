dbbd8f63d3b0d23db8b20d5f6745129b49666ab1
DedanKIarie <dedan.kiarie@student.moringaschool.com>
2025-05-15 14:49:42 +0300

    Update needle.py

--- needle.py
+++ needle.py
@@ -4,7 +4,7 @@ found = ""
 
 def findNeedle():
     if needle in junk:
-        found = f"found the needle at position {junk.index(needle)+1}"
+        found = str(f"found the needle at position {junk.index(needle)+1}")
         print(found)
         return found
 findNeedle()
